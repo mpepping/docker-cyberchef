da720c4d52ce8e39ec716953203563e90a3abd00
Martijn Pepping <martijn.pepping@automiq.nl>
2018-12-16 12:53:13 +0100

    ci

---
+++ update-releases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+die(){ 
+  echo "$@" >&2; exit 1;
+}
+
+get_latest_sha(){
+  curl --silent "https://api.github.com/repos/$1/commits" | jq -r '.[0].sha'
+}
+
+CURRENT=$(cat ./master_sha)
+REMOTE=$(get_latest_sha "gchq/CyberChef")
+LENGTH=${#REMOTE}
+
+if [ $LENGTH -ne 40 ]; then
+  die "Probably couldn't fecth remote SHA"
+fi
+
+if [ "$CURRENT" != "$REMOTE" ]; then
+  echo -n "$REMOTE" > ./master_sha
+  #git commit -am "Updated from ${CURRENT} to ${REMOTE}"
+  #git push
+fi
+
